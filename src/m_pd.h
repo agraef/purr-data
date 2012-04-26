b4d2d5c722c51ac3da40dd4ca3dcd18f1f324614
Ivica Ico Bukvic <ico@vt.edu>
2012-04-26 10:22:01 -0400

    Version bump

--- src/m_pd.h
+++ src/m_pd.h
@@ -11,7 +11,7 @@ extern "C" {
 #define PD_MAJOR_VERSION 0
 #define PD_MINOR_VERSION 42
 #define PD_BUGFIX_VERSION 6
-#define PD_TEST_VERSION "extended-l2ork-20120305"
+#define PD_TEST_VERSION "extended-l2ork-20120426"
 
 /* old name for "MSW" flag -- we have to take it for the sake of many old
 "nmakefiles" for externs, which will define NT and not MSW */
