5f19c3e7da16f6aa8a5612b315672222e36489fa
Ivica Ico Bukvic <ico@vt.edu>
2012-05-22 09:37:55 -0400

    added PDL2ORK define to distinguish between pdl2ork capabilities and vanilla pd and thus make externals work with both pd vanilla and pd-l2ork

--- src/m_pd.h
+++ src/m_pd.h
@@ -11,7 +11,8 @@ extern "C" {
 #define PD_MAJOR_VERSION 0
 #define PD_MINOR_VERSION 42
 #define PD_BUGFIX_VERSION 6
-#define PD_TEST_VERSION "extended-l2ork-20120518"
+#define PD_TEST_VERSION "extended-l2ork-20120519"
+#define PDL2ORK
 
 /* old name for "MSW" flag -- we have to take it for the sake of many old
 "nmakefiles" for externs, which will define NT and not MSW */
