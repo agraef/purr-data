3f0135ea7be6222ea567795fb2f0e502590a133f
Ivica Ico Bukvic <ico@vt.edu>
2013-02-25 15:54:17 -0500

    fixed bug where pd-l2ork symlink for pd~ external did not get installed

--- packages/linux_make/generate_install_makefile.bash
+++ packages/linux_make/generate_install_makefile.bash
@@ -23,6 +23,7 @@ function print_dir ()
 
 function post_install ()
 {
+	echo -e "\tcp -f lib/pd-l2ork/pd-l2ork \$(prefix)/lib/pd-l2ork/pd-l2ork"
 	echo -e "\tcp -f /usr/local/lib/pd-l2ork/pd-l2ork.png /usr/share/icons/hicolor/128x128/apps/"
 	echo -e "\tcp -f /usr/local/lib/pd-l2ork/pd-l2ork-red.png /usr/share/icons/hicolor/128x128/apps/"
 	echo -e "\tcp -f /usr/local/lib/pd-l2ork/pd-l2ork-k12.png /usr/share/icons/hicolor/128x128/apps/"
