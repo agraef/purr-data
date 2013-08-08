9bcdde9f0f11120ba591d28677cee33a2f722a6f
Ivica Ico Bukvic <ico@vt.edu>
2013-08-08 13:48:37 -0400

    fixed regression in route

--- pd/src/x_connective.c
+++ pd/src/x_connective.c
@@ -596,7 +596,12 @@ static void route_list(t_route *x, t_symbol *sel, int argc, t_atom *argv)
             if (argc > 1 && argv[1].a_type == A_SYMBOL)
                 outlet_anything(e->e_outlet, argv[1].a_w.w_symbol,
                     argc-2, argv+2);
-            else outlet_list(e->e_outlet, 0, argc-1, argv+1);
+            else {
+				if (argc > 1)
+					outlet_list(e->e_outlet, 0, argc-1, argv+1);
+				else
+					outlet_list(e->e_outlet, 0, argc, argv);
+			}
             return;
         }
     }
@@ -673,6 +678,7 @@ static void *route_new(t_symbol *s, int argc, t_atom *argv)
         SETFLOAT(&a, 0);
         argv = &a;
     }
+	x->x_type = argv[0].a_type;
     x->x_nelement = argc;
     x->x_vec = (t_routeelement *)getbytes(argc * sizeof(*x->x_vec));
     for (n = 0, e = x->x_vec; n < argc; n++, e++)
@@ -688,7 +694,6 @@ static void *route_new(t_symbol *s, int argc, t_atom *argv)
     }
     if (argc == 1)
     {
-		x->x_type = argv[0].a_type;
 		route_proxy_init(&x->x_pxy, x);
 		inlet_new(&x->x_obj, &x->x_pxy.l_pd, 0, 0);
         /*if (argv->a_type == A_FLOAT)
