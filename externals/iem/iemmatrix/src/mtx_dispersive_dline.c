7733387460fa6d5ff1393b47daafcdc29d526a6b
Jonathan Wilkes <jon.w.wilkes@gmail.com>
2015-11-08 18:28:23 -0500

    add function return types to get it to build on OSX

--- externals/iem/iemmatrix/src/mtx_dispersive_dline.c
+++ externals/iem/iemmatrix/src/mtx_dispersive_dline.c
@@ -141,7 +141,7 @@ static void mtx_dispersive_dline_resize(t_mtx_dispersive_dline *x, t_symbol *s,
    }
 }
 
-static allpass_chain_cycle (t_float x, t_float *y, t_float *z, int n, t_float a) {
+static void allpass_chain_cycle (t_float x, t_float *y, t_float *z, int n, t_float a) {
 	t_float w, in;
         int c;
 	in = y[0] = x;
@@ -153,7 +153,7 @@ static allpass_chain_cycle (t_float x, t_float *y, t_float *z, int n, t_float a)
 	}
 }
 
-static mtx_dispersive_dline_matrix(t_mtx_dispersive_dline *x, t_symbol *s,
+static void mtx_dispersive_dline_matrix(t_mtx_dispersive_dline *x, t_symbol *s,
       int argc, t_atom *argv)
 {
   int channels=(int)atom_getfloat(argv);
