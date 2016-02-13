31b9c51a0b3e16a09bd39f19287717495928807f
Jonathan Wilkes <jon.w.wilkes@gmail.com>
2016-02-12 20:11:28 -0500

    remove tkpath

--- pd/tkpath/demos/all.tcl
+++
@@ -1,1 +0,0 @@
-set dir [file dirname [info script]]set tail [file tail [info script]]foreach fileName [glob -nocomplain -directory $dir *.tcl] {    if {[file tail $fileName] ne $tail} {	source $fileName    }}
--- pd/tkpath/macosx/pkgIndex.tcl
+++
@@ -1,1 +0,0 @@
-package ifneeded QuickTimeTcl 3.1 [list load [file join $dir QuickTimeTcl3.1.dylib]]
