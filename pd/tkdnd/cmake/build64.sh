303888a89e32179b46fa6c6abf62546c17f157bc
Jonathan Wilkes <jon.w.wilkes@gmail.com>
2016-02-12 20:37:16 -0500

    remove tkdnd

--- pd/tkdnd/cmake/build64.sh
+++
@@ -1,9 +0,0 @@
-mkdir debug-nmake-x86_64
-cd    debug-nmake-x86_64
-cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Debug -D PKG_OS_ARCH=x86_64 -D CMAKE_INSTALL_PREFIX=../runtime ../..
-cd ..
-
-mkdir release-nmake-x86_64
-cd    release-nmake-x86_64
-cmake -G "Unix Makefiles" -D CMAKE_BUILD_TYPE=Release -D PKG_OS_ARCH=x86_64 -D CMAKE_INSTALL_PREFIX=../runtime ../..
-cd ..
--- pd/tkdnd/library/pkgIndex.tcl
+++
@@ -1,3 +0,0 @@
-package ifneeded tkdnd 2.5 \
-  "source \{$dir/tkdnd.tcl\} ; \
-   tkdnd::initialise \{$dir\} libtkdnd2.5[info sharedlibextension] tkdnd"
