b8cae2dd409163b25f0817ce0b59976ecc736576
Jonathan Wilkes <jon.w.wilkes@gmail.com>
2016-02-26 17:15:28 -0500

    more issues revealed by jslint

--- pd/nw/pdlang.js
+++ pd/nw/pdlang.js
@@ -1,17 +1,18 @@
+"use strict";
+
 var lang = require('./locales/en/translation.json');
 
 exports.lang = lang;
-
-exports.get_local_string = function(key) {
-    return recursive_key_splitter(key, lang);
-}
 
 function recursive_key_splitter(key, object) {
     var subkeys = key.split(".");
     if (subkeys.length > 1) {
         return recursive_key_splitter(subkeys.slice(1).join("."), object[subkeys[0]]);
-    }
-    else {
+    } else {
         return object[subkeys[0]];
     }
 }
+
+exports.get_local_string = function (key) {
+    return recursive_key_splitter(key, lang);
+};
