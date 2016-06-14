83511b66d36d07345e7034a40156a509906fddeb
Jonathan Wilkes <jon.w.wilkes@gmail.com>
2016-06-14 16:48:31 -0400

    port 498ff4ab6a23f510c9874d9d23cb28892e236312 from Pd-l2ork: added wiringPi as a submodule

--- l2ork_addons/raspberry_pi/disis_gpio/build.sh
+++ l2ork_addons/raspberry_pi/disis_gpio/build.sh
@@ -1,6 +1,5 @@
 curdir=`pwd`
-git clone git://git.drogon.net/wiringPi
-cd wiringPi/ && git pull && cd wiringPi/ && make static
+cd wiringPi/ && git stash && git pull && cd wiringPi/ && make static
 cd $curdir
 make
 exit 0
