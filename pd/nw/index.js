c1131fc02d41dc2731eabc067e1791463dc7ac3c
Jonathan Wilkes <jon.w.wilkes@gmail.com>
2016-10-12 02:00:47 -0400

    remove reference to tofront/back menu items accidentally left in after 8ae594377f7c128aea96907ba39f496792b577f1

--- pd/nw/index.js
+++ pd/nw/index.js
@@ -438,8 +438,6 @@ function nw_create_pd_window_menus(gui, w) {
     }
     if (osx) {
         minit(m.edit.tidyup, { enabled: false });
-        minit(m.edit.tofront, { enabled: false });
-        minit(m.edit.toback, { enabled: false });
         minit(m.edit.font, { enabled: false });
         minit(m.edit.cordinspector, { enabled: false });
     }
