2154d84324965be3603fea0992dfe225800c0567
Albert Graef <aggraef@gmail.com>
2016-12-03 21:56:46 +0100

    Cosmetic changes.

--- pd/src/g_scalar.c
+++ pd/src/g_scalar.c
@@ -544,24 +544,20 @@ void scalar_drawselectrect(t_scalar *x, t_glist *glist, int state)
         scalar_getbasexy(x, &basex, &basey);
 
         scalar_getrect(&x->sc_gobj, glist, &x1, &y1, &x2, &y2);
+        x1--; x2++; y1--; y2++;
         if (glist_istoplevel(glist))
         {
             t_float xorig = glist_xtopixels(glist, 0);
             t_float yorig = glist_ytopixels(glist, 0);
             t_float xscale = glist_xtopixels(glist, 1) - xorig;
             t_float yscale = glist_ytopixels(glist, 1) - yorig;
-            // the bbox is in scaled canvas coordinates, but
-            // gui_scalar_draw_select_rect expects them in unscaled units, so
-            // we undo the scaling (as well as the translation of the origin)
-            // here
+            // unscaled x/y coordinates
             t_float u1 = (x1 - xorig) / xscale;
             t_float v1 = (y1 - yorig) / yscale;
             t_float u2 = (x2 - xorig) / xscale;
             t_float v2 = (y2 - yorig) / yscale;
-            // undoing the scaling will reverse the order of coordinates if
-            // the corresponding scale factor is negative, so we put them back
-            // into ascending order if necessary; gui_scalar_draw_select_rect
-            // expects them that way
+            // make sure that these are in the right order,
+            // gui_scalar_draw_select_rect expects them that way
             if (u2 < u1) {
                 t_float u = u2;
                 u2 = u1; u1 = u;
@@ -573,9 +569,7 @@ void scalar_drawselectrect(t_scalar *x, t_glist *glist, int state)
             gui_vmess("gui_scalar_draw_select_rect", "xsiffffff",
                 glist_getcanvas(glist), tagbuf,
                 state,
-                // the actual selection rectangle is 1 unit larger than the
-                // bbox at each border
-                u1-1.0, v1-1.0, u2+1.0, v2+1.0,
+                u1, v1, u2, v2,
                 basex,
                 basey);
         }
