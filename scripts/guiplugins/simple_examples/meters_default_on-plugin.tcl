abc3fd29355752e763456c39f553edac4581d41e
Jonathan Wilkes <jon.w.wilkes@gmail.com>
2017-03-25 20:38:24 -0400

    repo cleanup: remove tcl/tk build infrastructure and scripts, old patches, automated build scripts for pd-extended, old gui plugins, and unnecessary install-time dependencies

--- scripts/guiplugins/simple_examples/meters_default_on-plugin.tcl
+++
@@ -1,3 +0,0 @@
-# this makes the meters in the Pd window be on by default
-set ::meters 1
-pdsend "pd meters 1"
--- scripts/guiplugins/simple_examples/set_canvas_minsize-plugin.tcl
+++
@@ -1,8 +0,0 @@
-
-# set the minimum size of canvas windows, so it doesn't hide the menubar
-if {$::windowingsystem ne "aqua"} {
-    set ::canvas_minwidth 310
-    set ::canvas_minheight 45
-}
-
-
