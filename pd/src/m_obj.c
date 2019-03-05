00a89d96dbf652450e7ef8cb3c63425e8c17e827
Jonathan Wilkes <jon.w.wilkes@gmail.com>
2019-03-04 21:42:31 -0500

    reduce stack size by one order of magnitude

--- pd/src/m_obj.c
+++ pd/src/m_obj.c
@@ -335,7 +335,7 @@ void obj_init(void)
 /* --------------------------- outlets ------------------------------ */
 
 static int stackcount = 0; /* iteration counter */
-#define STACKITER 10000 /* maximum iterations allowed */
+#define STACKITER 1000 /* maximum iterations allowed */
 
 static int outlet_eventno;
 
