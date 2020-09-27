01d7b4780f52599c2e0ee168f0bd6c596df82afa
Guillem <guillembartrina@gmail.com>
2020-09-27 16:10:18 +0200

    fixed dialog height

--- pd/nw/pdgui.js
+++ pd/nw/pdgui.js
@@ -6018,7 +6018,7 @@ function gui_external_dialog(did, external_name, attr_array) {
 function gui_abstractions_dialog(cid, gfxstub, filebased_abs, private_abs) {
     var attrs = { canvas: cid, filebased_abs: filebased_abs,
                     private_abs: private_abs };
-    dialogwin[gfxstub] = create_window(gfxstub, "abstractions", 300, 600,
+    dialogwin[gfxstub] = create_window(gfxstub, "abstractions", 300, Math.min(600, (private_abs.length*10 + 215)),
         0, 0, attrs);
 }
 
