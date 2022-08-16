9cb86f2b27cede405895aac1de793e3957bb1fe1
Albert Graef <aggraef@gmail.com>
2022-08-16 10:59:46 +0200

    Enable relevance order by default.

--- pd/src/s_file.c
+++ pd/src/s_file.c
@@ -43,8 +43,8 @@
 #endif
 
 int sys_defeatrt, sys_autopatch_yoffset, sys_snaptogrid = 1, sys_gridsize = 10,
-    sys_zoom,
-    sys_autocomplete = 1, sys_autocomplete_prefix, sys_autocomplete_relevance,
+    sys_zoom, sys_autocomplete = 1, sys_autocomplete_prefix,
+    sys_autocomplete_relevance = 1,
     sys_browser_doc = 1, sys_browser_path, sys_browser_init;
 t_symbol *sys_flags = &s_;
 void sys_doflags( void);
