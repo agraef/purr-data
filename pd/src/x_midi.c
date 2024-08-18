0a975738c8adbbe6e2db46332cbee62def0c354a
Albert Graef <aggraef@gmail.com>
2024-08-18 15:02:06 +0200

    Remove some bogus float args from the raw MIDI input objects.

--- pd/src/x_midi.c
+++ pd/src/x_midi.c
@@ -64,13 +64,13 @@ static void midiin_setup(void)
 {
     midiin_class = class_new(gensym("midiin"), (t_newmethod)midiin_new,
         (t_method)midiin_free, sizeof(t_midiin),
-            CLASS_NOINLET, A_DEFFLOAT, 0);
+            CLASS_NOINLET, 0);
     class_addlist(midiin_class, midiin_list);
     class_sethelpsymbol(midiin_class, gensym("midi"));
 
     sysexin_class = class_new(gensym("sysexin"), (t_newmethod)sysexin_new,
         (t_method)sysexin_free, sizeof(t_midiin),
-            CLASS_NOINLET, A_DEFFLOAT, 0);
+            CLASS_NOINLET, 0);
     class_addlist(sysexin_class, midiin_list);
     class_sethelpsymbol(sysexin_class, gensym("midi"));
 }
@@ -540,7 +540,7 @@ static void midirealtimein_setup(void)
 {
     midirealtimein_class = class_new(gensym("midirealtimein"),
         (t_newmethod)midirealtimein_new, (t_method)midirealtimein_free,
-            sizeof(t_midirealtimein), CLASS_NOINLET, A_DEFFLOAT, 0);
+            sizeof(t_midirealtimein), CLASS_NOINLET, 0);
     class_addlist(midirealtimein_class, midirealtimein_list);
         class_sethelpsymbol(midirealtimein_class, gensym("midi"));
 }
