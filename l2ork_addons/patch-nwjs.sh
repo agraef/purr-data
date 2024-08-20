8e1418ee2bb3552ff5a1e8ba2d1daf8d8e763f6b
Albert Graef <aggraef@gmail.com>
2024-08-21 01:54:43 +0200

    patch-nwjs.sh: Add -nw1 and -nw2 options.
    These options let you modify the nw.js configuration in an existing Purr
    Data installation in order to switch NW2 mode on or off. NW2 mode
    (https://nwjs.io/blog/nw2-mode/) is nw.js' new browser window
    implementation.
    
    CAVEAT: For unknown reasons this option negatively impacts nw.js'
    JavaScript exection performance, which slows down Purr Data's user
    interface *a lot*. Until this bug gets fixed, it is not recommended to
    use NW2 with Purr Data, and the latest packages we ship have it disabled.
    
    On the other hand, nw.js versions 0.68.0 and later *require* NW2 mode to
    work properly. Thus, if you absolutely need or want to use one of these
    later nw.js versions with Purr Data, then you will also have to switch
    NW2 mode back on (-nw2), and suffer the consequences.

--- l2ork_addons/patch-nwjs.sh
+++ l2ork_addons/patch-nwjs.sh
@@ -25,22 +25,56 @@
 # then use the default version that our packages ship with (0.67.0 at the time
 # of this writing). Or run ./patch-nwjs.sh -h to print some usage information.
 
-# NOTE: The script currently only works on Linux and on Windows using the msys
-# shell. On Mac, the nw.js package will be downloaded and extracted, but then
-# leaves you to do the rest, because I haven't yet figured out what goes where
-# exactly on the Mac; it's complicated. But if you're lucky, replacing just
-# /Applications/Purr-Data/Contents/MacOS/nwjs with the downloaded binary in
-# nw/nwjs.app/Contents/MacOS/nwjs might do the trick.
+# The script also lets you edit the package.json file of the installed
+# application to quickly switch NW2 mode on or off. Note that NW2 mode
+# (https://nwjs.io/blog/nw2-mode/) is required for Purr Data's window
+# management to work properly with any nw.js version after 0.67.1. But it
+# slows down JavaScript execution quite a lot, so that Purr Data's user
+# interface may become slow and unresponsive. Thus enabling this options isn't
+# recommended, and the latest packages we ship have it disabled. (If you're
+# still running an older Purr Data release and experience these slow-downs,
+# you can run ./patch-nwjs.sh -w1 to fix this.)
 
-nwjs_version="0.67.0"
+# To use this operation, you invoke the script with the -nw1 or -nw2 option,
+# indicating whether NW2 mode should be disabled or not. The -nw1 or -nw2
+# option can also be combined with the nw.js version argument if you want to
+# change both the version and the NW2 switch at the same time. Note that using
+# the -nw1 option with nw.js versions prior to 0.42.4 is possible but
+# unnecessary, since older versions have the NW2 option disabled by default.
+
+# To perform its operations, the script requires administrator access so that
+# it is able to modify the installed application. On Linux and Mac, the script
+# uses sudo for this purpose. On Windows, the script employs the gsudo program
+# (https://gerardog.github.io/gsudo/), which is a sudo replacement for Windows
+# systems which works nicely with msys2 bash. If you don't have this installed
+# then the automatic installation will fail and you will have to install
+# things manually.
+
+nwjs_version=""
+nwopt=""
+
 case "$1" in
+    -nw1) nwopt=nw1; shift;;
+    -nw2) nwopt=nw2; shift;;
+    [0-9]*) nwjs_version="$1"; shift;;
+esac
+
+case "$1" in
+    -nw1) nwopt=nw1;;
+    -nw2) nwopt=nw2;;
     [0-9]*) nwjs_version="$1" ;;
     "") ;;
-    *) echo "USAGE: $0 [-h] [nwjs-version]" 1>&2
+    *) echo "USAGE: $0 [-h] [-nw1|-nw2] [nwjs-version]" 1>&2
        echo "-h: print this message" 1>&2
+       echo "-nw1: switch NW2 mode OFF (https://nwjs.io/blog/nw2-mode/)" 1>&2
+       echo "-nw2: switch NW2 mode ON (this may slow down the application!)" 1>&2
        echo "nwjs-version (x.y.z): nw.js version to be used (default: 0.67.0)" 1>&2
        exit 0 ;;
 esac
+
+if [ -z "$nwopt" -a -z "$nwjs_version" ]; then
+    nwjs_version="0.67.0"
+fi
 
 os=`uname | tr '[:upper:]' '[:lower:]'`
 if [[ $os == *"mingw64"* ]]; then
@@ -79,6 +113,19 @@ if [[ $os == "win" || $os == "win64" || $os == "osx" ]]; then
 else
     ext="tar.gz"
 fi
+
+if [[ $os == "win" || $os == "win64" ]]; then
+    targetdir="$PROGRAMFILES/Purr Data/bin"
+    nwjsconfig="$LOCALAPPDATA/purr-data"
+elif [[ $os == "osx" ]]; then
+    targetdir="/Applications/Purr-Data.app"
+    nwjsconfig="$HOME/Library/Application Support/purr-data"
+else
+    targetdir="/opt/purr-data/lib/pd-l2ork/bin"
+    nwjsconfig="$HOME/.config/purr-data"
+fi
+
+if [ -n "$nwjs_version" ]; then
 
 nwjs="nwjs-sdk"
 if [[ $os == "win64" ]]; then
@@ -123,38 +170,24 @@ else
     find nw -type d -exec chmod a+x {} +
 fi
 
-if [[ $os == "win" || $os == "win64" ]]; then
-    targetdir="$PROGRAMFILES/Purr-Data/bin"
-    nwjsconfig="$LOCALAPPDATA/purr-data"
-elif [[ $os == "osx" ]]; then
-    targetdir="/Applications/Purr-Data.app"
-    nwjsconfig="$HOME/Library/Application Support/purr-data"
-else
-    targetdir="/opt/purr-data/lib/pd-l2ork/bin"
-    nwjsconfig="$HOME/.config/purr-data"
-fi
-
 # Get rid of the previous nw.js config data which may cause issues when
 # downgrading the nw.js version.
 # (cf. https://agraef.github.io/purr-data-intro/Purr-Data-Intro.html#where-are-my-configuration-files).
 echo "Removing previous $nwjsconfig. It will be re-created."
 rm -rf "$nwjsconfig"
 
+echo "Replacing nw.js in $targetdir now."
+echo "You may be prompted to enter your administrator password."
+echo "NOTE: Pressing Ctr+C leaves the nw folder in the current directory."
+echo "NOTE: You can then install it manually or remove it."
+
 if [[ $os == "win" || $os == "win64" ]]; then
-    echo "Replacing nw.js in $targetdir now."
-    echo "You may be prompted to enter your administrator password."
-    echo "NOTE: Pressing Ctr+C leaves the nw folder in the current directory."
-    echo "NOTE: You can then install it manually or remove it."
     echo "NOTE: This requires gsudo (https://gerardog.github.io/gsudo/)."
     echo "NOTE: Otherwise the automatic installation will fail."
     echo "Continue? (Press Return to continue, Ctrl+C to abort.)"
     read
     /c/Program\ Files/gsudo/Current/gsudo.exe "/bin/bash" -c "rm -rf /c/Program\\ Files/Purr\\ Data/bin/nw && mv nw /c/Program\\ Files/Purr\\ Data/bin"
 elif [[ $os == "osx" ]]; then
-    echo "Replacing nw.js in $targetdir now."
-    echo "You may be prompted to enter your sudo password."
-    echo "NOTE: Pressing Ctr+C leaves the nw folder in the current directory."
-    echo "NOTE: You can then install it manually or remove it."
     echo "Continue? (Press Return to continue, Ctrl+C to abort.)"
     read
     # This is complicated. On Mac, the zip contains an entire application
@@ -176,13 +209,62 @@ elif [[ $os == "osx" ]]; then
     # part of the bundle, which we don't need):
     rm -rf nw
 else
-    echo "Replacing nw.js in $targetdir now."
-    echo "You may be prompted to enter your sudo password."
-    echo "NOTE: Pressing Ctr+C leaves the nw folder in the current directory."
-    echo "NOTE: You can then install it manually or remove it."
     echo "Continue? (Press Return to continue, Ctrl+C to abort.)"
     read
     sudo rm -rf "$targetdir/nw"
     sudo chown -R root:root nw
     sudo mv nw "$targetdir"
 fi
+
+fi #  -n "$nwjs_version"
+
+# edit the package.json file
+
+if [[ $os == "osx" ]]; then
+    targetdir="/Applications/Purr-Data.app/Contents/Resources/app.nw"
+    # Mac sed has its peculiarities...
+    sed="sed -i ''"
+else
+    # GNU sed
+    sed="sed -i"
+fi
+
+pkgfile="$targetdir/package.json"
+
+if [ -z "$nwopt" ]; then
+    echo "Done."
+    exit 0
+elif [ ! -f "$pkgfile" ]; then
+    echo 1>&2 "-$nwopt specified, but $pkgfile could not be found."
+    exit 1
+fi
+
+if ! cp "$pkgfile" .; then
+    echo 1>&2 "Couldn't create package.json. Exiting."
+    exit 1
+fi
+
+case "$nwopt" in
+    nw1) echo "Turning NW2 mode OFF (https://nwjs.io/blog/nw2-mode/).";
+	 echo "You may be prompted to enter your administrator password.";
+	 $sed 's/"--proxy-server=/"--disable-features=nw2 --proxy-server=/' package.json;;
+    nw2) echo "Turning NW2 mode ON (https://nwjs.io/blog/nw2-mode/).";
+	 echo "You may be prompted to enter your administrator password.";
+	 echo "WARNING: This may SLOW DOWN the application's user interface.";
+	 $sed 's/"--disable-features=nw2 /"/' package.json;;
+esac
+
+if [[ $os == "win" || $os == "win64" ]]; then
+    echo "NOTE: This requires gsudo (https://gerardog.github.io/gsudo/)."
+    echo "NOTE: Otherwise the automatic installation will fail."
+    echo "Continue? (Press Return to continue, Ctrl+C to abort.)"
+    read
+    /c/Program\ Files/gsudo/Current/gsudo.exe "/bin/bash" -c "rm -f /c/Program\\ Files/Purr\\ Data/bin/package.json && mv package.json /c/Program\\ Files/Purr\\ Data/bin"
+else
+    echo "Continue? (Press Return to continue, Ctrl+C to abort.)"
+    read
+    sudo rm -f "$pkgfile"
+    sudo mv package.json "$targetdir"
+fi
+
+echo "Done."
